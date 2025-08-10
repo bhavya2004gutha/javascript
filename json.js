280258fa9876837d3ec00637c0d161dbeace52a8
bhavya2004gutha <bhavya.gutha@tekreant.com>
2025-08-10 16:37:15 +0530

    Add files via upload

---
+++ json.js
@@ -0,0 +1,13 @@
+
+// // json string
+// const jsonStr = '{"name": "Alice"}';
+// const obj = JSON.parse(jsonStr);
+// console.log(obj.name); 
+
+// // object to string
+// const person = { name: "Bhavya", age: 20 };
+// const jsonString = JSON.stringify(person);
+// console.log(jsonString); 
+
+const jsonString = JSON.stringify(person);
+console.log(jsonString);
