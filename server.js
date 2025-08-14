603a40150f837e61e6fe3b975459156b86857f9b
bhavya2004gutha <bhavya.gutha@tekreant.com>
2025-08-14 23:16:54 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,38 +1,25 @@
-// const express = require("express")
-// const dotEnv = require("dotenv")
-
-// const app = express();
-// const port = 3000;
-
-// app.listen(port,() =>{
-//     console.log ('sever started running at ${port}')
-// })
-
-
-
 
 const express = require("express")
 const dotEnv = require("dotenv")
-const { MongoClient } = require("mongodb");
-const bodyparser = require("body-parser")
-const employeeroutes = require('/routes/employeeroutes')
+const mongoose=require("mongoose")
+
+const routes = require('../mongodb/Routes/employeeRoutes')
 
 const app = express();
 dotEnv. config ()
-const PORT = process.env.PORT || 5000
-MongoClient.connect(process.env.mongo_url)
+app.use(express.json())
+const PORT = 5500
+
+mongoose.connect(process.env.mongo_url)
 .then(() =>{
     console.log("Mongodb connected sucessfully")
 })
 .catch((error) =>{
     console.log("Error",error)
 })
-const port = 3000;
 
-
-
-app.listen(port,() =>{
-    console.log ('sever started running at ${port}')
+app.listen(PORT,() =>{
+    console.log (`sever started running at ${PORT}`)
 })
 
-
+app.use("/main",routes)
