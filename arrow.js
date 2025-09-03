385aa2b4414629fcd53a0c501ebd8330c84a845c
bhavya2004gutha <bhavya.gutha@tekreant.com>
2025-09-03 18:41:21 +0530

    Add files via upload

---
+++ arrow.js
@@ -0,0 +1,17 @@
+const add = (a, b) => a + b;
+console.log(add(3, 4)); 
+
+
+
+function multiply(a,b) {
+    const result =  a*b;
+    return result;
+
+
+} 
+console.log(multiply(3,4));
+
+
+const subract = (a,b) =>a-b ;
+console.log(subract(10,4));
+
